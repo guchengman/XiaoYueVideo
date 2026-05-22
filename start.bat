485dc183eadff4d0a57015d5d767d61ab6dd6078
Admin <admin@xiaoyue.shop>
2026-05-22 20:27:33 +0800

    clear .nuxt cache on startup to avoid vite import errors

--- start.bat
+++ start.bat
@@ -7,6 +7,9 @@ if not exist "cookies" mkdir cookies
 if not exist "temp\downloads" mkdir temp\downloads
 if not exist "logs" mkdir logs
 
+REM Clear Nuxt/Vite cache to avoid stale import errors
+if exist ".nuxt" rmdir /s /q ".nuxt"
+
 echo Starting XiaoYueVideo dev server...
 echo.
 npx nuxi dev
